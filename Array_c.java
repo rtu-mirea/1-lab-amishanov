c10b68ecb14f29368cf425f2747b2a70d9752eca
amishanov <55013597+amishanov@users.noreply.github.com>
2019-09-21 09:51:10 +0300

    Add files via upload

---
+++ Array_c.java
@@ -0,0 +1,55 @@
+package com.company;
+import java.util.Scanner;
+
+public class Array_c {
+    int count;
+    byte arr[];
+    Array_c(int count){
+        this.count=count;
+        arr = new byte [count];
+    }
+    void rand_Input (){//Метод получения псевдо случайного числа
+        for (int i = 0; i < count; i++){
+            arr[i] = (byte) ((Math.random()*255)-128) ;
+        }
+    }
+
+    void Input(){ //Метод ввода в консоль
+        System.out.println("Введите " + count + " Чисел");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите элементы массива");
+        for (int i = 0; i < count; i++){
+            arr[i] = in.nextByte();
+        }
+    }
+    void Out(){
+        System.out.println("Ваш массив: ");
+        for (int i:arr){
+            System.out.println(i);
+        }
+    }
+    void revers_Out(){
+        System.out.println("Ваш массив: ");
+        for (int i=count-1; i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+    void swap(){
+        int mid= count / 2;
+        for (int i=0; i<mid;i++) {
+            byte temp = arr[i];
+            arr[i] = arr[count - 1 - i];
+            arr[count - 1 - i]=temp;
+        }
+        //this.Out();
+    }
+    void guess_task(byte pos,byte val){
+      for (int i=0; i<count;i++){
+           if (val==1)
+               arr[i]=(byte) (arr[i] | (1<<pos));
+           else
+               arr[i]=(byte) (arr[i] & ~(1<<pos));
+        }
+        //this.Out();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,54 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Array_c a=new Array_c(1);
+        int n, num=0;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Операции над массивом");
+        System.out.println("1. Заполнить случаными");
+        System.out.println("2. Заполнить вручную");
+        System.out.println("3. Вывод массива");
+        System.out.println("4. Обратный вывод");
+        System.out.println("5. Перевернуть массив");
+        System.out.println("6. Заменить выбранный бит на выбранное значение");
+        while (num!=7){
+            System.out.println("Выберите операцию");
+            num=in.nextInt();
+            switch (num){
+                case 1:
+                    System.out.println("Введите количество элементов");
+                    n=in.nextInt();
+                    a=new Array_c(n);
+                    a.rand_Input();
+                    break;
+                case 2:
+                    System.out.println("Введите количество элементов");
+                    n=in.nextInt();
+                    a=new Array_c(n);
+                    a.Input();
+                    break;
+                case 3:
+                    a.Out();
+                    break;
+                case 4:
+                    a.revers_Out();
+                    break;
+                case 5:
+                    a.swap();
+                    break;
+                case 6:
+                    byte p, v;
+                    System.out.println("Введите номер бита (0;7]");
+                    p=in.nextByte();
+                    System.out.println("Введите номер бита 0 или 1");
+                    v=in.nextByte();
+                    a.guess_task(p,v);
+                    break;
+                default:
+                    System.out.println("Неверно");
+            }
+        }
+    }
+}
